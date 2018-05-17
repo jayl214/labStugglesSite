a7017e54c362aa21334d00a5053294853ff89cc2
jayl214 <jay.lin@mail.mcgill.ca>
2018-05-17 16:17:44 -0400

    test popups layer

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -11,22 +11,51 @@ $(document).ready(()=>{
 
 
   var marker1 = L.marker([45.5048, -73.5772]).addTo(mymap);
-  var marker2 = L.marker([45.5048, -3.5672]).addTo(mymap);
+  var marker2 = L.marker([45.5048, -73.2772]).addTo(mymap);
+
+  // marker1.bindPopup("<b>Hello world!</b><br>I am a popup1.")
+  // marker2.bindPopup("<b>Hello world!</b><br>I am a popup2.")
+
+  // function instaPopup(coordArray, content){
+
+  // }
+
+  var popupLayer = [
+    L.popup()
+      .setLatLng([45.5048, -73.5772])
+      .setContent('<h1>popup1<h1>'),
+    L.popup()
+      .setLatLng([45.5048, -73.2772])
+      .setContent("popup2")
+  ]
 
 
-  marker2.bindPopup("<b>Hello world!</b><br>I am a popup2.")
-  marker1.bindPopup("<b>Hello world!</b><br>I am a popup1.")
+
+  // var popup1 = L.popup()
+  //   .setLatLng([45.5048, -73.5772])
+  //   .setContent("I am a standalone popup.")
+  //   // .openOn(mymap);
+
+  // var popup2 = L.popup()
+  //   .setLatLng([45.5048, -73.2772])
+  //   .setContent("I am a standalone popup.")
+  //   // .openOn(mymap);
+
+  // mymap.addLayer(popup1).addLayer(popup2);
+
 
   mymap.on("zoomend", function(){
     zoomLev = mymap.getZoom();
     console.log(zoomLev)
-    // if (zoomLev < 14){
-    //   map.removeLayer(lariac);
-    //   $('#zoom').val(zoomLev);
-    // }else{
-    //   $('#zoom').val(zoomLev);
-    //   map.addLayer(lariac);
-    // }
+    if (zoomLev > 5){
+      popupLayer.forEach((popup)=>{
+        mymap.addLayer(popup)
+      })
+    }else if(zoomLev < 6){
+      popupLayer.forEach((popup)=>{
+        mymap.removeLayer(popup)
+      })
+    }
   });
 
 

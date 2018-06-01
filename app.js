15d01e89049b2c6cfe8c820bd943d00b49ed5c62
jayl214 <jay.lin@mail.mcgill.ca>
2018-06-01 00:31:28 -0400

    http rerout https

--- app.js
+++ app.js
@@ -20,9 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('*', (req, res) => {
-  res.redirect('https://' + req.headers.host + req.url);
-})
+app.use(function(req, res, next) {
+  if(!req.secure) {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  next();
+});
 
 //require routes
 require('./server/routes')(app);

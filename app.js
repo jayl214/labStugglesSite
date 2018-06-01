8e59aa3a51a87666ac64bf6625c38518f9470e11
jayl214 <jay.lin@mail.mcgill.ca>
2018-06-01 00:35:00 -0400

    undo last

--- app.js
+++ app.js
@@ -20,13 +20,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-  if(!req.secure) {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  next();
-});
-
 //require routes
 require('./server/routes')(app);
 

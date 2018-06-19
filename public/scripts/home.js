0ae92a2ad3e3042580d25deb8b27ee2655bee177
jayl214 <jay.lin@mail.mcgill.ca>
2018-06-18 21:05:45 -0400

    front page styling

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,5 +1,18 @@
 $(document).ready(()=>{
-  $(".map").click(()=>{
 
+
+  $(".home-comic").on("mouseenter", ()=>{
+    $(".home-comic").find("img").attr('src','docs/images/buttoncloud1hover.png')
+  })
+  $(".home-comic").on("mouseleave", ()=>{
+    $(".home-comic").find("img").attr('src','docs/images/buttoncloud1.png')
+  })
+
+
+  $(".home-foster").on("mouseenter", ()=>{
+    $(".home-foster").find("img").attr('src','docs/images/buttoncloud2hover.png')
+  })
+  $(".home-foster").on("mouseleave", ()=>{
+    $(".home-foster").find("img").attr('src','docs/images/buttoncloud2.png')
   })
 })

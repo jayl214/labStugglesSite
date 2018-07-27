4a3c4abfd5386bbe6e8a8d3141aec920a9edeeb6
jayl214 <jay.lin@mail.mcgill.ca>
2018-07-27 16:03:44 -0400

    config.js production typo fix

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,8 @@ module.exports = {
   //   dialect: 'psql'
   // },
   production: {
-    development: {
     // url: process.env.PRODUCTION_DATABASE_URL,
     url: process.env.DATABASE_URL,
     dialect: 'postgres'
-  },
   }
 };

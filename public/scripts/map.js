800aa3e227a777749fd15c0a155bf7d696e59ae0
jayl214 <jay.lin@mail.mcgill.ca>
2018-07-29 13:46:10 -0400

    restyling foster page

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,7 +2,7 @@
 $(document).ready(()=>{
 
   // initialize map with default coords and zoom level
-  let mymap = L.map('mapid', {scrollWheelZoom: false,} ).setView([45.5048, -73.5772], 4)
+  let mymap = L.map('map', {scrollWheelZoom: false} ).setView([45.5048, -73.5772], 3)
 
   //restrict panning to world map
   const southWest = L.latLng(-89.98155760646617, -180), northEast = L.latLng(89.99346179538875, 180);
@@ -15,7 +15,7 @@ $(document).ready(()=>{
   // mapbox tiles
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 15,
+    maxZoom: 6,
     minZoom: 2,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiamF5bDIxNCIsImEiOiJjamhhNjd4bjIwbDRzM2NtZmhoZXhpbndmIn0.j8Nl7tQahZWIPei_CuuO5w'
@@ -63,7 +63,6 @@ $(document).ready(()=>{
         }else{ //if the coords already exist, append data of new post to existing obj
           let existingPopup = uniquePopupObj[`${coord}`]
 
-          // // makes popup fatter if more than one post
           // existingPopup.singleOrManyPost = "many-post"
 
           existingPopup["posts"].push({
@@ -92,49 +91,56 @@ $(document).ready(()=>{
                                     </div>`
 
         //push leaflet-ready data to popupLayer array
-        popupLayer.push(setPopup(uniquePopupObj[key].coord,uniquePopupObj[key].popup))
+        // popupLayer.push(setPopup(uniquePopupObj[key].coord,uniquePopupObj[key].popup))
       }
 
       // place markers at locations of popups
       for (let key in uniquePopupObj){
-        placeMarkers(uniquePopupObj[key].coord)
+        let marker = placeMarkers(uniquePopupObj[key].coord)
+        marker.bindPopup(setPopup(uniquePopupObj[key].popup), {closeOnClick: false, autoClose: true}).openPopup()
+        // marker.bindTooltip(uniquePopupObj[key].popup, {permanent: false, autoClose: false})
+        // let marker = L.marker(uniquePopupObj[key].coord).addTo(mymap)
       }
 
       //toggle popup on zoom level
-      mymap.on("zoomend", function(){
-        zoomLevel = mymap.getZoom();
-        if (zoomLevel > 3){
-          popupLayer.forEach((popup)=>{
-            if(!popup.isOpen()){
-              mymap.addLayer(popup)
-            }
-          })
-        }else if(zoomLevel < 4){
-          popupLayer.forEach((popup)=>{
-            if(popup.isOpen()){
-              mymap.removeLayer(popup)
-            }
-          })
-        }
-      })
+      // mymap.on("zoomend", function(){
+      //   zoomLevel = mymap.getZoom();
+      //   if (zoomLevel > 3){
+      //     popupLayer.forEach((popup)=>{
+      //       if(!popup.isOpen()){
+      //         mymap.addLayer(popup)
+      //       }
+      //     })
+      //   }else if(zoomLevel < 4){
+      //     popupLayer.forEach((popup)=>{
+      //       if(popup.isOpen()){
+      //         mymap.removeLayer(popup)
+      //       }
+      //     })
+      //   }
+      // })
 
-      popupLayer.forEach((popup)=>{
-            mymap.addLayer(popup)
-          })
+      // popupLayer.forEach((popup)=>{
+      //       mymap.addLayer(popup)
+      //     })
 
     } // ajax req success close
   }) //ajax close
 
   //place marker at coord
   function placeMarkers(coordSet){
-    L.marker(coordSet).addTo(mymap)
+    return L.marker(coordSet, {riseOnHover:true}).addTo(mymap)
   }
 
   //place popup containing html at coord
-  function setPopup(coordSet, content){
+  // function setPopup(coordSet, content){
+  function setPopup(content){
     return L.popup()
-              .setLatLng(coordSet)
+              // .setLatLng(coordSet)
               .setContent(content)
+              .on('click',
+                console.log('clicked')
+              )
   }
 
 //document ready
